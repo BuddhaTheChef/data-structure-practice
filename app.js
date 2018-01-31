df0e1debd072280f4e2eb0d5c41a1f92b3dc98cc
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-31 10:07:50 -1000

    Day 2 data structures continued

--- app.js
+++ app.js
@@ -65,16 +65,62 @@ LinkedList.prototype.addToHead = function (value) {
     this.tail = newNode;
     }
     this.head = newNode;
-}
+};
 
-var LL = new LinkedList();
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if(this.tail) {
+    this.tail.next = newNode;
+  }
+  else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+};
 
-LL.addToHead(100);
-LL.addToHead(200);
-LL.addToHead(300);
-LL.addToHead(400);
+LinkedList.prototype.removeHead = function () {
+  if(!this.head) return null;
+  var val = this.head.value;
+  this.head = this.head.next;
+  if(this.head) {
+    this.head.prev = null;
+  }
+  else {
+    this.tail = null;
+  }
+  return val;
+};
 
-console.log(LL);
+LinkedList.prototype.removeTail = function () {
+  if(!this.tail) return null;
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if(this.tail) {
+    this.tail.next = null;
+  }
+  else {
+    this.head = null;
+  }
+  return val;
+ };
+
+LinkedList.prototype.search = function (searchValue) {
+  var currentNode = this.head;
+  while(currentNode) {
+    if(currentNode.value === searchValue) return currentNode.value;
+    currentNode = currentNode.next;
+  }
+  return null;
+};
+
+var myLL = new LinkedList();
+myLL.addToHead(123);
+myLL.addToHead(70);
+myLL.addToHead('hello');
+myLL.addToTail(19);
+myLL.addToTail('world');
+myLL.addToTail(20);
+
 
 
 //////////////////////////////////////////////////////////////////////////////////
